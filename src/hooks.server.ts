cac6345be6c28b070e7c625adc16bc0c36d869ac
JacobLinCool <jacoblincool@gmail.com>
2023-09-02 04:28:46 +0800

    feat: upgrade to svelte 4

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,5 @@
-import { locale } from "svelte-i18n";
 import type { Handle } from "@sveltejs/kit";
+import { locale } from "svelte-i18n";
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const lang = event.request.headers.get("accept-language")?.split(",")[0] || "en";
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { t } from "svelte-i18n";
 </script>
 
-<div class="h-full w-full flex justify-center items-center">
+<div class="flex h-full w-full items-center justify-center">
 	<div class="prose">
 		<h1>{$t("welcome")}</h1>
 		<p>{$t("with-tech")}</p>
